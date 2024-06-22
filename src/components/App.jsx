aabaf59ca524766fbdc6c4e665b280af74474a32
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-06-22 22:01:58 +0800

    Initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,51 @@
 // rcc is the shortcut broiler plate for class component
 
-import { Component } from "react"
+import { useState } from "react"
 import { Section } from "./Section/Section"
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions"
 import { Notification } from "./Notification/Notification"
 
-export default class App extends Component {
+function App() {
 
-  state = {
-  Good: 0,
-  Neutral: 0,
-  Bad: 0,
-  isClicked: false
-  }
+  const [status, setStatus] = useState({
+    Good: 0,
+    Neutral: 0,
+    Bad: 0
+  })
+  const [isClicked, setIsClicked] = useState(false)
+  // state = {
+  // Good: 0,
+  // Neutral: 0,
+  // Bad: 0,
+  // isClicked: false
+  // }
   
-  render() {
     const options = ['Good', 'Neutral', 'Bad']
-    const { Good, Neutral, Bad, isClicked } = this.state
+    const { Good, Neutral, Bad } = status
 
     const countTotalFeedback = () => {
       return Good + Neutral + Bad
-    }
+  }
+  
     const countPositiveFeedbackPercentage = () => {
       return Math.floor((Good / total) * 100)
-    }
-    const total = countTotalFeedback()
+  }
+  
+  const total = countTotalFeedback()
+  
     const positivePercentage = countPositiveFeedbackPercentage()
     
-    const onLeaveFeedback = (option) => {
+  const onLeaveFeedback = (option) => {
       
-      this.setState(prevValue => ({
-        ...prevValue,
-        [option]: prevValue[option] + 1,
-        isClicked: true
-      }))
-    }
+    setStatus(prevValue => ({
+      ...prevValue,
+      [option]: prevValue[option] + 1,
+    }))
+
+    setIsClicked(true)
+  }
+
     return (
-      // <div>
-      //   <Section title="" options={options} onLeaveFeedback={onLeaveFeedback} good={good} neutral={neutral} bad={bad} isClicked={isClicked} total={total} positivePercentage={positivePercentage}/>
-      // </div>
       <div>
         <Section title="Please leave feedback">
           <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
@@ -50,5 +57,7 @@ export default class App extends Component {
       </div>
       
     )
-  }
+  
 }
+
+export default App;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -5,8 +5,6 @@ export const Section = ({ title, children }) => {
 
 return (
     <div>
-        {/* <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
-        <Notification message="There is no feedback" good={good} neutral={neutral} bad={bad} total={total} isClicked={isClicked} positivePercentage={positivePercentage}/> */}
         <h1>{title}</h1>
         <section>{children}</section>
         
@@ -14,16 +12,6 @@ return (
 )
 }
 
-// Section.propTypes = {
-//     options: PropTypes.arrayOf(PropTypes.string).isRequired,
-//     onLeaveFeedback: PropTypes.func.isRequired,
-//     good: PropTypes.number.isRequired,
-//     neutral: PropTypes.number.isRequired,
-//     bad: PropTypes.number.isRequired,
-//     isClicked: PropTypes.bool.isRequired,
-//     total: PropTypes.number.isRequired,
-//     positivePercentage: PropTypes.number.isRequired
-// }
 
 Section.propTypes = {
     title: PropTypes.string.isRequired,
